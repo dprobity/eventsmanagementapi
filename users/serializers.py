83c015e600e82867251d46e0d095b55b793f7f8d
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-03-25 07:11:08 -0500

    feat(users): implement custom user model with JWT authentication and profile endpoints

---
+++ users/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.validators import UniqueValidator
+
+User = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        min_length=8,
+        validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password', 'password2', 'is_organizer')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match"})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')  # Remove confirm password field
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'is_organizer', 'date_joined')
+        read_only_fields = ('id', 'email', 'date_joined')
